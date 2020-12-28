274d32f373632436d69d66a0d51fca6ae320bfe9
Rajat <dey.rajat33@gmail.com>
2020-12-28 21:59:32 +0530

    recyclerview added with adapter and basic layout.
    Scrollview is also added.
    Recyclerview scroll horizontally

--- app/src/main/java/com/example/edtech/MainActivity.java
+++ app/src/main/java/com/example/edtech/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.edtech;
 
-import android.os.Bundle;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+        import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
+
+
+        import android.view.KeyEvent;
+        import android.view.MenuItem;
+
+
+        import java.util.ArrayList;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
@@ -24,6 +34,192 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        venueadapter customAdapter = new venueadapter(MainActivity.this, city(),seat(), ground_name(),home_ground());
+        mRecyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
+        customAdapter.notifyDataSetChanged();
+
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view2);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        venueadapter2 customAdapter2 = new venueadapter2(MainActivity.this, city(),seat(), ground_name(),home_ground());
+        mRecyclerView.setAdapter(customAdapter2); // set the Adapter to RecyclerView
+        customAdapter2.notifyDataSetChanged();
+
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view3);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        venueadapter3 customAdapter3 = new venueadapter3(MainActivity.this, city(),seat(), ground_name(),home_ground());
+        mRecyclerView.setAdapter(customAdapter3); // set the Adapter to RecyclerView
+        customAdapter3.notifyDataSetChanged();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
+    public ArrayList<String> city() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Bengaluru");
+        list.add("Delhi");
+        list.add("Hyderabad ");
+        list.add("Indore");
+        list.add("Jaipur");
+        list.add("Kolkata");
+        list.add("Mohali");
+        list.add("Mumbai");
+        list.add("Pune");
+        list.add("Chennai");
+
+        return list;
+    }
+
+    public ArrayList<String> seat() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Seating Capacity: 35,000");
+        list.add("Seating Capacity: 41,820");
+        list.add("Seating Capacity: 39,408");
+        list.add("Seating Capacity: 30,000");
+        list.add("Seating Capacity: 23,185");
+        list.add("Seating Capacity: 68,000");
+        list.add("Seating Capacity: 26,000");
+        list.add("Seating Capacity: 33,000");
+        list.add("Seating Capacity: 42,000");
+        list.add("Seating Capacity: 33,500");
+
+        return list;
+    }
+
+    public ArrayList<String> ground_name() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("M. Chinnaswamy Stadium");
+        list.add("Feroz Shah Kotla Ground");
+        list.add("Rajiv Gandhi Intl. Cricket Stadium");
+        list.add("Holkar Cricket Stadium");
+        list.add("Sawai Mansingh Stadium");
+        list.add("Eden Gardens");
+        list.add("IS Bindra Stadium");
+        list.add("Wankhede Stadium");
+        list.add("Maharashtra Cricket Association's International Stadium");
+        list.add("M. A. Chidambaram Stadium");
+
+        return list;
+    }
+
+    public ArrayList<String> home_ground() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Royal Challengers Bangalore");
+        list.add("Delhi Capitals");
+        list.add("Sunrisers Hyderabad");
+        list.add("Kings XI Punjab");
+        list.add("Rajasthan Royals");
+        list.add("Kolkata Knight Riders");
+        list.add("Kings XI Punjab");
+        list.add("Mumbai Indians");
+        list.add("-");
+        list.add("Chennai Super Kings");
+
+        return list;
+    }
+
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/edtech/venueadapter.java
@@ -0,0 +1,70 @@
+package com.example.edtech;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class venueadapter extends RecyclerView.Adapter<venueadapter.MyViewHolder> {
+
+    private ArrayList<String> city ;
+    private ArrayList<String> seat;
+    private ArrayList<String> ground_name;
+    private ArrayList<String> home_ground;
+
+    private Context context;
+
+    public venueadapter(Context context, ArrayList<String> city, ArrayList<String> seat, ArrayList<String> ground_name,ArrayList<String> home_ground) {
+        this.context = context;
+        this.city = city ;
+        this.seat= seat;
+        this.ground_name = ground_name;
+        this.home_ground =home_ground;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // infalte the item Layout
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.ipl_venue_layout, parent, false);
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in ite
+        holder.city  .setText(city.get(position));
+        holder.seat  .setText(seat.get(position));
+        holder.ground_name.setText(ground_name.get(position));
+        holder.home_ground .setText(home_ground.get(position));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return ground_name.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView city,seat,ground_name,home_ground;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            // get the reference of item view's
+            city         = (TextView) itemView.findViewById(R.id.city);
+            seat         = (TextView) itemView.findViewById(R.id.seat );
+            ground_name  = (TextView) itemView.findViewById(R.id.ground_name);
+            home_ground  = (TextView) itemView.findViewById(R.id.home_ground);
+
+        }
+    }
+}
+
